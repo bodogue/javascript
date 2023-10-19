8f8796a1358e7b1c4368bb41b0fcd2f7c2882e53
bodogue <137304409+bodogue@users.noreply.github.com>
2023-10-19 10:56:14 -0300

    novos exercicios

---
+++ exercicios/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60
+console.log(`Voce esta a ${vel}km/h.`)
+if (vel > 60) {
+    console.log(`Voce ULTRAPASSOU a velocidade permitida. MULTADO!`)
+}
---
+++ exercicios/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}, sou `)
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro!')
+}
