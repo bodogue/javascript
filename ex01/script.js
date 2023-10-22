e3c06fe9acba8a44f2e26d1ef768e38323d27fb5
bodogue <137304409+bodogue@users.noreply.github.com>
2023-10-21 21:27:38 -0300

    adicionei os minutos

--- ex01/script.js
+++ ex01/script.js
@@ -3,18 +3,21 @@ var dia = document.querySelector('#dia')
 var tarde = document.querySelector('#tarde')
 var noite = document.querySelector('#noite')
 var display = document.querySelector('#display')
-var hora = new Date()
-var agora = hora.getHours()
-if (agora < 12) { // DIA
-    display.innerHTML = `Agora são <strong>${agora} horas</strong>. Bom dia!`
+var data = new Date()
+var agoraHor = data.getHours()
+var agoraMin = data.getMinutes()
+var agoraDia = data.getDay()
+var agoraMes = data.getMonth()
+if (agoraHor < 12) { // DIA
+    display.innerHTML = `Agora são <strong>${agoraHor}:${agoraMin}</strong>. Bom dia!`
     dia.style.display = 'block'
     fundo.style.backgroundColor = '#D0D631'
-} else if (agora < 18) { // TARDE
-    display.innerHTML = `Agora são <strong>${agora} horas</strong>. Boa tarde!`
+} else if (agoraHor < 18) { // TARDE
+    display.innerHTML = `Agora são <strong>${agoraHor}:${agoraMin}</strong>. Boa tarde!`
     tarde.style.display = 'block'
     fundo.style.backgroundColor = '#D1A872'
 } else { // NOITE
-    display.innerHTML = `Agora são <strong>${agora} horas</strong>. Boa noite!`
+    display.innerHTML = `Agora são <strong>${agoraHor}:${agoraMin}</strong>. Boa noite!`
     noite.style.display = 'block'
     fundo.style.backgroundColor = '#70616C'
 }
