6528b01dfddd68512348747324143720ef10af80
bodogue <137304409+bodogue@users.noreply.github.com>
2023-10-25 14:56:23 -0300

    coloquei option no ex4 e tirei css desnecessario do ex2

--- ex04/arquivos/script.js
+++ ex04/arquivos/script.js
@@ -1,19 +1,16 @@
 function gerar() {
     var num = document.querySelector('#num')
-    var res = document.querySelector('#res')
-    var s = 0
-    var n = Number(num.value)
-    res.innerHTML = ''
+    var tab = document.querySelector('#tab')
     if (num.value.length == 0) {
         alert('Por favor, digite um valor.')
     } else {
-        for (var c = 1; c <= 10; c ++) {
-            s = c * n
-            if (c - 10) {
-                res.innerHTML += `${n} x ${c} = ${s} <br>`
-            } else {
-                res.innerHTML += `${n} x ${c} = ${s} <br>`
-            }
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for (var c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text += `${n} x ${c} = ${c*n}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
         }
     }
 }
